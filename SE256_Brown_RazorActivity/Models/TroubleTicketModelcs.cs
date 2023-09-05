af9a83eced5f17740322488dd07a4f0f03e14d40
Brown6734 <clbrown1@email.neit.edu>
2023-09-05 16:19:27 -0400

    Finished ASP.NET activity 6. Razor pages and validation activity done.

--- SE256_Brown_RazorActivity/Models/TroubleTicketModelcs.cs
+++ SE256_Brown_RazorActivity/Models/TroubleTicketModelcs.cs
@@ -19,18 +19,40 @@ namespace SE256_Brown_RazorActivity.Models
 
         [Required]
         
+        [StringOptionsValidate(Allowed = new String[] { "Monitor", "Computer", "Printer", "Software Install", "Software Upgrade", "Configuration", "Internet" }, ErrorMessage = "Sorry...Category is invalid. Categories: Monitor, Computer, Printer, Software Install, Software Upgrade, Configuration, Internet")]
+
+
         public String Category { get; set; }
 
+
+        [Required, EmailAddress]
         public String Reporting_Email { get; set; }
 
+
+        [Required]
+        [Display(Name = "Original date of the problem.")]
+        [DataType(DataType.DateTime), DisplayFormat(DataFormatString = "{0:MM/dd/yyyy hh:mm:ss tt}", ApplyFormatInEditMode = true)]
+        [MyDate(ErrorMessage = "Future data entry not allowed")]
+
         public DateTime Orig_Date { get; set; }
+
+
+        [Required]
+        [Display(Name = "Date of solutions/closure")]
+        [DataType(DataType.DateTime), DisplayFormat(DataFormatString = "{0:MM/dd/yyyy hh:mm:ss tt}", ApplyFormatInEditMode = true)]
+
 
         public DateTime Close_Date { get; set; }
 
         public String Responder_Notes { get; set; }
 
+
+        [EmailAddress]
+
         public String Responder_Email { get; set; }
 
+
+        [Required]
         public bool Active { get; set; }
     }
 }
